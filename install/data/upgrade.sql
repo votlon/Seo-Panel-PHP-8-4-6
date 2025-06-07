e8bca2ec210a8fe4eae8c9069ad0b7f89e04b749
D2 <11322006+votlon@users.noreply.github.com>
2025-06-06 22:58:15 -0400

    Update version constants to 8.4.6

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -1,7 +1,6 @@
---
--- Seo Panel 4.12.0 changes
+-- Seo Panel 8.4.6 changes
 --
 
-update `settings` set set_val='4.12.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
+update `settings` set set_val='8.4.6' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
 UPDATE searchengines SET url = REPLACE(url, 'http://', 'https://') WHERE url LIKE 'http://%';
